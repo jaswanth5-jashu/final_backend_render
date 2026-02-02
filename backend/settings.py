6f151b7e76284dd8b35cfacc2fb6de97c6fbbe4e
Jaswanth Simha <jaswanthsimha2004@gmail.com>
2026-02-02 19:39:05 +0530

    first

--- backend/settings.py
+++ backend/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
         },
     },
 ]
-'''
+
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
@@ -69,30 +69,7 @@ else:
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
         }
-    }'''
-
-DATABASE_URL = os.environ.get("DATABASE_URL", "").strip()
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
     }
-}
-
-if DATABASE_URL:
-    try:
-        DATABASES["default"] = dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            ssl_require=DATABASE_URL.startswith("postgres"),
-        )
-    except Exception as e:
-        print("⚠️ Invalid DATABASE_URL:", DATABASE_URL)
-        print("⚠️ Reason:", e)
-
-
-
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
